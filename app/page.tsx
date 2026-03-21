faeab8792bdb0d37b5c0262187275bb0756906e5
AaronFrancis05 <aarontaremwa8@gmail.com>
2026-03-21 16:56:05 +0300

    Navigation setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import Nav from'@/components/nav/nav'
 
 export default function Home() {
   return (
-    <main>
+    <main className={"min-h-screen bg-[#DED5D5] "}>
+        <Nav />
         <h1> Home</h1>
     </main>
   );
---
+++ lib/constant.ts
@@ -0,0 +1,17 @@
+export const NAV_ITEMS = [
+  {
+    label: "Projects",
+    icon: "/assets/icons/icons8-briefcase-50.png",
+    path: "/projects",
+  },
+  {
+    label: "Services",
+    icon: "/assets/icons/icons8-job-50.png",
+    path: "/services",
+  },
+  {
+    label: "Pricing",
+    icon: "/assets/icons/icons8-online-payment-with-a-credit-card-50.png",
+    path: "/pricing",
+  },
+];
